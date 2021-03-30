61c843cf472f27b5fcd92fa1d050837d23ad18e3
Mubasher <21694@student.dorset-college.ie>
2021-03-30 14:42:05 +0100

    first

---
+++ src/com/company/Login.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Login {
+
+    String username;
+    String password;
+
+    public Login(String username, String password){
+
+        this.username = username;
+        this.password = password;
+
+    }
+}
